db212f12c4e1942aa896a59a4e40bd1e09490f35
Adishwar Rishi <adiswa123@gmail.com>
2023-05-20 13:15:25 +1000

    Fix unit tests

--- src/drizzle.test.ts
+++ src/drizzle.test.ts
@@ -6,9 +6,11 @@ const describe = setupMiniflareIsolatedStorage();
 
 describe("D1 DB test", () => {
   let workerEnv: Env;
+  let ctx: ExecutionContext;
 
   beforeEach(async () => {
     workerEnv = getMiniflareBindings();
+    ctx = new ExecutionContext();
     await workerEnv.__D1_BETA__DB.exec(`
     CREATE TABLE "Address" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,"createdAt" text DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ', 'now', 'utc')) NOT NULL,"line1" text,"line2" text,"city" text,"state" text,"countryAlpha3" text NOT NULL,"postCode" text);
     CREATE TABLE "Customer" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,"createdAt" text DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ', 'now', 'utc')) NOT NULL,"updatedAt" text DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ', 'now', 'utc')) NOT NULL,"firstName" text NOT NULL,"lastName" text NOT NULL,"email" text NOT NULL,"addressId" integer NOT NULL,FOREIGN KEY ("addressId") REFERENCES "Address"("id") ON UPDATE cascade ON DELETE set null);
@@ -17,8 +19,8 @@ describe("D1 DB test", () => {
   });
 
   it("should successfully execute the join query", async () => {
-    const req = new Request("http://localhost/", { method: "GET" });
-    const res = await app.fetch(req, workerEnv);
+    const req = new Request("http://localhost/join-test", { method: "GET" });
+    const res = await app.fetch(req, workerEnv, ctx);
     expect(res.status).toBe(200);
 
     const json = (await res.json()) as any;
